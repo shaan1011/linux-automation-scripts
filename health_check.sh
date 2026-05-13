4a4337b5a976cfab571233021cf2cd5303242e3a
shaan1011 <shaan1011@gmail.com>
2026-05-13 23:44:18 +0530

    Create health_check.sh

---
+++ health_check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LOGFILE=system_checks.log
+
+echo "Performing System Health Checks..." >> $LOGFILE
+echo "Date: $(date)" >> $LOGFILE
+echo "Hostname: $(hostname)" >> $LOGFILE
+echo "Uptime: $(uptime)"
+echo -e "Disk uage: \n" >> $LOGFILE
+df -hT >> $LOGFILE
+echo -e "Memory Usgae: \n" >> $LOGFILE
+free -h >> $LOGFILE
+
+printf "This concludes the System Checks and we've covered below topics: \nDate \nHostname \nUptime \nDisk usage \nMemory Usgae"
+
