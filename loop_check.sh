6326c769b5dcc7a9c9b79dc991fef84c9c1831cd
shaan1011 <shaan1011@gmail.com>
2026-05-13 20:14:57 +0530

    Update loop_check.sh

--- loop_check.sh
+++ loop_check.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+COUNT=1
+while [ $COUNT -le 5 ]
+do
+echo "Iteration $COUNT"
+echo "Date: $(date)"
+echo "Uptime: $(uptime)"
+echo "Hostname: $(hostname)"
+(($COUNT++))
+done
+
+VALUE=5
+for (( i=1;i<=VALUE;i++ ))
+do
+echo "Iteration $i"
+echo "Date: $(date)"
+echo "Uptime: $(uptime)"
+echo "Hostname: $(hostname)"
+done
 
